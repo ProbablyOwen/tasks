6b6abd7e5025d5b9a1398cb24347ed6f4e1e939e
Owen Cook <sendowenmail@gmail.com>
2026-03-08 22:55:00 -0400

    finished 2 components

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [answerRevealed, setAnswerRevealed] = useState(false);
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setAnswerRevealed(!answerRevealed);
+                }}
+            >
+                Reveal answer
+            </Button>
+
+            {answerRevealed && <p>The answer is 42</p>}
+        </div>
+    );
 }
